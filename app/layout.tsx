c18fa56fd7e0b2969cf83f29fbbdfebf5708a9d4
Rodjensky <pitonrodjy@gmail.com>
2026-07-01 11:51:39 -0400

    Ajout de la vérification Google

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,10 @@ import SessionTimeout from "@/components/SessionTimeout";
 export const metadata: Metadata = {
   title: "FlexiPass | Cartes Cadeaux Numériques",
   description: "Offrez le choix avec nos cartes numériques instantanées.",
+
+  verification: {
+    google: "HO3jLj3vt59YwRDX0BFE4h0Nuc7GdKQz88XzjKTY0q4",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -19,7 +23,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </head>
       <body
         style={{
-          "--font-sans": "Inter, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', sans-serif",
+          "--font-sans":
+            "Inter, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', sans-serif",
         } as React.CSSProperties}
       >
         <AuthCallbackHandler />
